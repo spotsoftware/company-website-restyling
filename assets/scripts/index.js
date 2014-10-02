4acc202fd1635040959c709cfb62b1daa68b8e0c
Andrea Rinaldi <andrea.rinaldi@spot.it>
2014-10-02 18:21:11 +0200

    header

---
+++ assets/scripts/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.skills-wrapper .skills').bxSlider({
+        mode: 'vertical',
+        auto: true,
+        minSlides: 1,
+        responsive: true,
+        touchEnabled: true,
+        pager: false,
+        controls: false,
+        useCSS: false,
+        pause: 10000
+    });
+});

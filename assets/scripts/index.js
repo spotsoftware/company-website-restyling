63ce2aa3b14632c2c5c6496179484e9a218347c0
Andrea Rinaldi <andrea.rinaldi@spot.it>
2014-10-02 19:14:28 +0200

    sass version of bootstrap and introducing navbar

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,38 @@
 $(document).ready(function() {
+    /**
+     *   Global variables.
+     */
+    var pageHeight = $(window).height();
+    var pageWidth = $(window).width();
+    var navigationHeight = $("#navigation").outerHeight();
+
+    /**
+     *   ON RESIZE, check again
+     */
+    $(window).resize(function() {
+        pageWidth = $(window).width();
+        pageHeight = $(window).height();
+    });
+
+    $(window).trigger('scroll');
+    
+    /* Fix navigation. */
+    $('div.navbar').fixedonlater({
+        speedDown: 250,
+        speedUp: 100
+    });
+
+    /* Scroll spy and scroll filter */
+    $('#main-menu').onePageNav({
+        currentClass: "active",
+        changeHash: false,
+        scrollOffset: navigationHeight - 10,
+        scrollThreshold: 0.5,
+        scrollSpeed: 750,
+        filter: "",
+        easing: "swing"
+    });
+
     $('.skills-wrapper .skills').bxSlider({
         mode: 'vertical',
         auto: true,
